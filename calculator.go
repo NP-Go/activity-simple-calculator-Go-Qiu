8f765b60e822ea88bd3ba83ef4ae87f46519bc93
Dylan Kiew <das.dylan.qiu@gmail.com>
2022-03-28 20:39:49 +0800

    implemented code for the calculator

--- calculator.go
+++ calculator.go
@@ -4,23 +4,29 @@ import "fmt"
 
 func add(a, b int) int {
 	//Insert code here
-
+	return a + b
 }
 
 func subtract(a, b int) int {
 	//Insert code here
-
+	return a - b
 }
 
 func multiply(a, b int) int {
 	//Insert code here
-
+	return a * b
 }
 
 func divide(a, b int) int {
 	//Insert code here
 	//consider for b = 0
+	outcome := 0
 
+	if b != 0 {
+		outcome = a / b
+	}
+
+	return outcome
 }
 
 func main() {
@@ -34,4 +40,29 @@ func main() {
 	fmt.Scanln(&b)
 
 	//Insert code here
+	if process == "add" {
+		outcome := add(a, b)
+		fmt.Printf("%d add %d is %d\n", a, b, outcome)
+	}
+
+	if process == "sub" {
+		outcome := subtract(a, b)
+		fmt.Printf("%d subtract %d is %d\n", a, b, outcome)
+	}
+
+	if process == "mul" {
+		outcome := multiply(a, b)
+		fmt.Printf("%d multiply %d is %d\n", a, b, outcome)
+	}
+
+	if process == "div" {
+
+		if b == 0 {
+			fmt.Println("The divisor cannot be a zero.")
+		} else {
+			outcome := divide(a, b)
+			fmt.Printf("%d divide %d is %d\n", a, b, outcome)
+		}
+	}
+
 }
